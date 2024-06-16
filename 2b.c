f4613cc8c416136c061063053fb0f898e9a65097
PRANAV R <231401077@rajalakshmi.edu.in>
2024-06-16 21:24:44 +0530

    Update 2b.c

--- 2b.c
+++ 2b.c
@@ -31,3 +31,42 @@ int main() {
         displayStudentDetails(&students[i]);
         printf("\n");
 }
+    struct Student topStudent = findStudentWithHighestMarks(students, n);
+    printf("Student with highest total marks:\n");
+    displayStudentDetails(&topStudent);
+
+    return 0;
+}
+void readStudentDetails(struct Student* student) {
+    printf("Name: ");
+    scanf("%s", student->name);
+    printf("Roll Number: ");
+    scanf("%d", &student->rollNumber);
+    printf("Marks in three subjects: ");
+    for (int i = 0; i < 3; ++i) {
+        scanf("%d", &student->marks[i]);
+    }
+}
+void calculateTotalAndAverageMarks(struct Student* student) {
+    student->totalMarks = 0;
+    for (int i = 0; i < 3; ++i) {
+        student->totalMarks += student->marks[i];
+    }
+    student->averageMarks = student->totalMarks / 3.0;
+}
+void displayStudentDetails(const struct Student* student) {
+    printf("Name: %s\n", student->name);
+    printf("Roll Number: %d\n", student->rollNumber);
+    printf("Total Marks: %d\n", student->totalMarks);
+    printf("Average Marks: %.2f\n", student->averageMarks);
+}
+struct Student findStudentWithHighestMarks(struct Student students[], int n) {
+    struct Student topStudent = students[0];
+for (int i = 1; i < n; ++i) {
+        if (students[i].totalMarks > topStudent.totalMarks) {
+            topStudent = students[i];
+        }
+    }
+    return topStudent;
+}
+
